081bf2a114d96e90ec71b3fa885158064bc26722
danielecamagnadhdk <daniele.camagna@studio.unibo.it>
2026-02-22 15:50:10 +0100

    Update main.py

--- network_analysis/main.py
+++ network_analysis/main.py
@@ -1,10 +1,10 @@
 import networkx as nx
 from na import *
 
-G=full_analysis(file="data/edge_list_owen.csv",
-                division=deaths,
-                only_main_characters=True,
-                visualization=True,
-                analysis=False)
+G=full_analysis(file="data/edge_list_owen.csv",     #if you want wargrave and owen as the same node, you can imput the file edge_list.csv
+                division=deaths,                    #here you can also put division=blocks or division=chapters as parameters
+                only_main_characters=True,          #here you choose if the nodes are only the ten protagonists or not
+                visualization=True,                 #this creates the images in a folder entitled 'networks'
+                analysis=False)                     #this prints the measures results
 
-#nx.write_gexf(G, 'graph.gexf')         #FILE FOR GEPHI
+#nx.write_gexf(G, 'graph.gexf')  #If you activate this operation, you get a file you can put in GEPHI software and there you can perform further analysis
